622e56b7c7edec3ac8ed18888703a97adaa786b2
antoninhrlt <antoninhrlt1@gmail.com>
2022-03-23 08:38:15 +0100

    Update main module

--- sdymh/__init__.py
+++ sdymh/__init__.py
@@ -5,8 +5,11 @@
 import math
 
 # Importations to use with directly a class name : "sdymh.ClassName()"
+import sdymh.functions
+from sdymh.plane_geom import Point
+import sdymh.turtle 
 from sdymh.vectors import Vector
-from sdymh.plane_geom import Point 
+
 
 # Just a simple function to say thank you for this project
 # Link to "buy my coffee" in the response
@@ -41,3 +44,26 @@ def pythagore(a, b):
 # "x" it's the "right angle"
 def reciprocal_pythagore(a, b, c):
     return (a ** 2) + (b ** 2) == (c ** 2)
+
+
+# Return a list of all numbers of the table `n` from 0 to parameter `to`
+def mul_table(n, to=10, printing=True):
+    numbers = []
+    for i in range(1, to + 1):
+        if printing:
+            print(f"{n} x {i} = {i * n}")
+        else:
+            numbers += i * n
+    return numbers
+
+# Calculate the IMC of a person with their weight and height
+# If your height is "1m80" in real life, please give "1.80" as parameter
+def imc(weight: float, height: float):
+    return weight / (height**2)
+
+
+# Check if number `y` is a multiple of `x` 
+def is_multiple(x, y):
+    if x % y == 0:
+        return True
+    return False
