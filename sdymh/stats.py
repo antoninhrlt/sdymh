7e0c3bbd4c26bd1e2598581f1e039f9c8d566973
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-13 08:43:58 +0200

    New stats functions

--- sdymh/stats.py
+++ sdymh/stats.py
@@ -35,3 +35,20 @@ def generate_frequency_table(values):
         total += r
     
     print(f"{total}\t|")
+
+
+# Example :
+# You have 15 roses and it represents 60% percent of the flowers (total)
+# 60/100 x = 15 <=> 0.6x = 15 <=> x = 15/0.6 = 25
+# So, you have 25 flowers in total
+def total_from_percentage(percentage, equal, total_id="total", part_id="x", output=False):
+    total = 100 / percentage * equal
+    if output:
+        print(f"{percentage}% of {total_id} = {equal} {part_id}")
+        print(f"{total_id} = {total}")
+    return total
+
+
+# 100/400 = 1/4 = 0.25 = 25/100 = 25%
+def proportion_to_percentage(n, total):
+    return (n / total) * 100
